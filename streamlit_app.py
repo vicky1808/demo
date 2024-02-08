27a974f12d96224c90d6d67a05142835d21a568e
vicky1808 <159413240+vicky1808@users.noreply.github.com>
2024-02-08 15:22:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 st.title('IRIS FLOWER DASHBOARD')
 st.divider()
-df=pd.read_csv("C:/Users/User20/Downloads/Iris (1).csv")
+df=pd.read_csv("Iris (1).csv")
 st.sidebar.header('Description :::')
 st.sidebar.markdown('The Iris flower data set is a multivariate data set introduced by the British statistician and biologist Ronald Fisher in his 1936 paper The use of multiple measurements in taxonomic problems. It is sometimes called Anderson Iris data set because Edgar Anderson collected the data to quantify the morphologic variation of Iris flowers of three related species. The data set consists of 50 samples from each of three species of Iris (Iris Setosa, Iris virginica, and Iris versicolor). Four features were measured from each sample: the length and the width of the sepals and petals, in centimeters.')
 col1,col2=st.columns(2)
